d1a61172146fe79688ac9a50fad2ade4c9995aee
Mini Model <minimodel@metamodel.me>
2013-05-13 23:12:15 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_country/languages/el/tl_metamodel_attribute.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/el/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:59+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['countries']['0']         = 'Φιλτρο διαθεσιμων χωρων';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['countries']['1']         = 'Φιλτράρισμα των χώρων που θα είναι διαθέσιμες για επιλογή. Εάν δεν υπάρχει καμία επιλογή, όλες οι χώρες θα είναι διαθέσιμες. ';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['country'] = 'Χωρα';
