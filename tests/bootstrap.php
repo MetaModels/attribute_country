c39a1e583c1aa43517c98283bd7f53520d13deea
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-11-18 23:39:27 +0100

    Reorder folder strucure, reindention, etc.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,35 +6,33 @@
  * data in each collection.
  *
  * PHP version 5
- * @package    MetaModels
- * @subpackage Tests
- * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * 
+ * @package     MetaModels
+ * @subpackage  Tests
+ * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright   The MetaModels team.
+ * @license     LGPL.
  * @filesource
  */
-
 error_reporting(E_ALL);
 
 function includeIfExists($file)
 {
-	return file_exists($file) ? include $file : false;
+    return file_exists($file) ? include $file : false;
 }
 
 if (
-	// Locally installed dependencies
-	(!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
-	// We are within an composer install.
-	&& (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
-	echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
-		'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
-		'php composer.phar install'.PHP_EOL;
-	exit(1);
+// Locally installed dependencies
+(!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php')) && 
+// We are within an composer install.
+(!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))) {
+    echo 'You must set up the project dependencies, run the following commands:' . PHP_EOL . 'curl -sS https://getcomposer.org/installer | php' . PHP_EOL . 'php composer.phar install' . PHP_EOL;
+    exit(1);
 }
 
 $loader->add('MetaModels\Test', __DIR__);
 
 function utf8_romanize($string)
 {
-	return strtoupper($string);
+    return strtoupper($string);
 }
