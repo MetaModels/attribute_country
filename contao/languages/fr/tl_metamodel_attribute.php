f3a91ccabcd040c5fa5f8cf0cc8d2c83fd834953
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-11-18 23:38:59 +0100

    Reorder folder strucure, reindenting, etc.

---
+++ contao/languages/fr/tl_metamodel_attribute.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex.
+ * To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *      
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *         
+ *          last-updated: 2013-05-06T21:11:12+02:00
+ */
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['countries']['0'] = 'Filtrer les pays disponibles';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['countries']['1'] = 'Filtrer les pays qui seront disponibles pour la sélection. Si aucun n\'est sélectionné, tous les pays seront disponibles.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['country'] = 'Pays';
---
+++ src/MetaModels/Attribute/Country/Country.php
@@ -0,0 +1,232 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * 
+ * @package     MetaModels
+ * @subpackage  AttributeColor
+ * @author      Oliver Hoff <oliver@hofff.com>
+ * @copyright   The MetaModels team.
+ * @license     LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\Attribute\Country;
+
+use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
+use ContaoCommunityAlliance\Contao\Bindings\Events\System\LoadLanguageFileEvent;
+use MetaModels\Attribute\BaseSimple;
+use MetaModels\Render\Template;
+
+/**
+ * Attribute Country
+ */
+class Country extends BaseSimple {
+
+    /**
+     * Local lookup cache for country names in a given language.
+     *
+     * @var array
+     */
+    protected $countryCache = array();
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function prepareTemplate(Template $objTemplate, $arrRowData, $objSettings = null)
+    {
+        parent::prepareTemplate($objTemplate, $arrRowData, $objSettings);
+        $objTemplate->value = $this->getCountryLabel($arrRowData[$this->getColName()]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSQLDataType()
+    {
+        return 'varchar(2) NOT NULL default \'\'';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAttributeSettingNames()
+    {
+        return array_merge(
+            parent::getAttributeSettingNames(),
+            array(
+                'countries',
+                'filterable',
+                'searchable',
+                'sortable',
+                'flag',
+                'mandatory',
+                'includeBlankOption' 
+            )
+        );
+    }
+
+    /**
+     * Include the TL_ROOT/system/config/countries.php file and return the contained $countries variable.
+     *
+     * @return string[]
+     */
+    protected function getRealCountries()
+    {
+        // @codingStandardsIgnoreStart - Include is required here, can not switch to require_once.
+        include (TL_ROOT . '/system/config/countries.php');
+        // @codingStandardsIgnoreEnd
+        
+        /**
+         * @var string[] $countries
+         */
+        return $countries;
+    }
+
+    /**
+     * Retrieve all country names in the given language.
+     *
+     * @param string $language The language key.
+     *       
+     * @return string[]
+     */
+    protected function getCountryNames($language)
+    {
+        $dispatcher = $GLOBALS['container']['event-dispatcher'];
+        /**
+         * @var \Symfony\Component\EventDispatcher\EventDispatcher $dispatcher
+         */
+        $event = new LoadLanguageFileEvent('countries', $language, true);
+        $dispatcher->dispatch(ContaoEvents::SYSTEM_LOAD_LANGUAGE_FILE, $event);
+        
+        return $GLOBALS['TL_LANG']['CNT'];
+    }
+
+    /**
+     * Restore the normal language values.
+     *
+     * @return void
+     */
+    protected function restoreLanguage()
+    {
+        // Switch back to the original FE language to not disturb the frontend.
+        if ($this->getMetaModel()->getActiveLanguage() != $GLOBALS['TL_LANGUAGE']) {
+            $dispatcher = $GLOBALS['container']['event-dispatcher'];
+            $event = new LoadLanguageFileEvent('countries', null, true);
+            /**
+             * @var \Symfony\Component\EventDispatcher\EventDispatcher $dispatcher
+             */
+            $dispatcher->dispatch(ContaoEvents::SYSTEM_LOAD_LANGUAGE_FILE, $event);
+        }
+    }
+
+    /**
+     * Retrieve all country names.
+     *
+     * This method takes the fallback language into account.
+     *
+     * @return string[]
+     */
+    protected function getCountries()
+    {
+        $loadedLanguage = $this->getMetaModel()->getActiveLanguage();
+        if (isset($this->countryCache[$loadedLanguage])) {
+            return $this->countryCache[$loadedLanguage];
+        }
+        
+        $languageValues = $this->getCountryNames($loadedLanguage);
+        $countries = $this->getRealCountries();
+        $keys = array_keys($countries);
+        $aux = array();
+        $real = array();
+        
+        // Fetch real language values.
+        foreach ($keys as $key) {
+            if (isset($languageValues[$key])) {
+                $aux[$key] = utf8_romanize($languageValues[$key]);
+                $real[$key] = $languageValues[$key];
+            }
+        }
+        
+        // Add needed fallback values.
+        $keys = array_diff($keys, array_keys($aux));
+        if ($keys) {
+            $loadedLanguage = $this->getMetaModel()->getFallbackLanguage();
+            $fallbackValues = $this->getCountryNames($loadedLanguage);
+            foreach ($keys as $key) {
+                if (isset($fallbackValues[$key])) {
+                    $aux[$key] = utf8_romanize($fallbackValues[$key]);
+                    $real[$key] = $fallbackValues[$key];
+                }
+            }
+        }
+        
+        $keys = array_diff($keys, array_keys($aux));
+        if ($keys) {
+            foreach ($keys as $key) {
+                $aux[$key] = $countries[$key];
+                $real[$key] = $countries[$key];
+            }
+        }
+        
+        asort($aux);
+        $return = array();
+        foreach (array_keys($aux) as $key) {
+            $return[$key] = $real[$key];
+        }
+        
+        $this->restoreLanguage();
+        
+        $this->countryCache[$loadedLanguage] = $return;
+        
+        return $return;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFieldDefinition($arrOverrides = array())
+    {
+        $arrFieldDef = parent::getFieldDefinition($arrOverrides);
+        $arrFieldDef['inputType'] = 'select';
+        $arrFieldDef['eval']['chosen'] = true;
+        $arrFieldDef['options'] = $this->getCountries();
+        
+        $arrSelectable = deserialize($this->get('countries'), true);
+        $arrSelectable && $arrFieldDef['options'] = array_intersect_key($arrFieldDef['options'], array_flip($arrSelectable));
+        
+        return $arrFieldDef;
+    }
+
+    /**
+     * Retrieve the label for a given country.
+     *
+     * @param string $strCountry The country for which the label shall be retrieved.
+     *       
+     * @return string
+     */
+    public function getCountryLabel($strCountry)
+    {
+        $countries = $this->getCountries();
+        
+        return isset($countries[$strCountry]) ? $countries[$strCountry] : null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFilterOptions($arrIds, $usedOnly, &$arrCount = null)
+    {
+        $options = parent::getFilterOptions($arrIds, $usedOnly, $arrCount);
+        
+        foreach ($options as $k => $v) {
+            $options[$k] = $this->getCountryLabel($k);
+        }
+        
+        return $options;
+    }
+}
