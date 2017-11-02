d11d937b1557bda6fb29d62d58c3d17169fecad3
David Molineus <david.molineus@netzmacht.de>
2017-11-02 16:13:42 +0100

    Switch to psr-4 autoloading.

--- src/Country.php
+++ src/Country.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_country.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,8 @@
  * @author     Oliver Hoff <oliver@hofff.com>
  * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
  * @author     Tim Becker <tb@westwerk.ac>
- * @copyright  2012-2016 The MetaModels team.
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_country/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
